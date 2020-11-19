dd7f9d3d15eaa43886ba8c489d845ed1d39f4ce5
cmull1 <69908301+cmull1@users.noreply.github.com>
2020-11-19 00:02:45 -0500

    Update Spotify_Playlist_Create.py
    added code that gets users liked videos on youtube

--- Spotify_Playlist_Create.py
+++ Spotify_Playlist_Create.py
@@ -24,3 +24,36 @@ class CreatePlaylist:
             api_service_name, api_version, credentials=credentials)
 
         return youtube_client
+    
+     def get_liked_videos(self):
+        """Grab Our Liked Videos & Create A Dictionary Of Important Song Information"""
+        request = self.youtube_client.videos().list(
+            part="snippet,contentDetails,statistics",
+            myRating="like"
+        )
+        response = request.execute()
+
+        # collect each video and get important information
+        for item in response["items"]:
+            video_title = item["snippet"]["title"]
+            youtube_url = "https://www.youtube.com/watch?v={}".format(
+                item["id"])
+
+            # use youtube_dl to collect the song name & artist name
+            video = youtube_dl.YoutubeDL({}).extract_info(
+                youtube_url, download=False)
+            song_name = video["track"]
+            artist = video["artist"]
+
+            if song_name is not None and artist is not None:
+                # save all important info and skip any missing song and artist
+                self.all_song_info[video_title] = {
+                    "youtube_url": youtube_url,
+                    "song_name": song_name,
+                    "artist": artist,
+
+                    # add the uri, easy to get song to put into playlist
+                    "spotify_uri": self.get_spotify_uri(song_name, artist)
+
+                }
+
